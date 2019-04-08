8d876d9a643f0dabb775b80b67f5c9852158e17c
Vlad Bashkin <vbackskin@gmail.com>
2019-04-08 15:51:11 +0400

    111

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    private static String mainfxml = "res/sample.fxml";
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource(mainfxml));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+        /* test of repo cloning */
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
