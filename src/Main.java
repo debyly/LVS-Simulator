6497d6e28b7649d17e9895a4ae0f6a7bfee98d8c
backskin <vbackskin@gmail.com>
2019-05-05 22:11:18 +0400

    Пробуем запускать GUI (тест №1)

--- src/Main.java
+++ src/Main.java
@@ -9,32 +9,20 @@ import javafx.application.Application;
 
 public class Main extends Application {
 
-    /*private static String mainfxml = "res/sample.fxml";
+    private static String mainfxml = "view/MainScreen.fxml";
+
+
+    public static void main(String[] args) { launch(args); }
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
+
         Parent root = FXMLLoader.load(getClass().getResource(mainfxml));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-        *//* test of repo cloning *//*
-        //Another one shit
-        //second one
-        //I DON UBawfaw
 
-    }*/
-
-
-    public static void main(String[] args) {
-        launch(args);
-
-
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-            primaryStage.show();
-            LVS lvs = new LVS();
-            lvs.working_20000();
+        LVS lvs = new LVS();
+        lvs.working_20000();
     }
 }
