3c7f5dfec900a0b6ce39cb78958c29904343e062
backskin <vbackskin@gmail.com>
2019-05-10 21:18:56 +0400

    Пробуем запускать GUI (тест №2) Вывод инфы в визуал-консоль

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,13 @@ import view.MainScreen;
 
 public class Main extends Application {
 
-    private static String mainfxml = "view/MainScreen.fxml";
-
 
     public static void main(String[] args) { launch(args); }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        String mainfxml = "view/MainScreen.fxml";
         FXMLLoader loader = new FXMLLoader(getClass().getResource(mainfxml));
         Parent root = loader.load();
         MainScreen MSController = loader.getController();
