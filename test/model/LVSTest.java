5295808e33253f2d0c08cca2892f36995389f786
backskin <vbackskin@gmail.com>
2019-05-29 20:43:02 +0400

    Курсач 1.3 build 3 + возможность изменить кол-во ОУ

---
+++ test/model/LVSTest.java
@@ -0,0 +1,31 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LVSTest {
+
+    @Test
+    void setLineState() {
+    }
+
+    @Test
+    void realLVS() {
+    }
+
+    @Test
+    void testLVS() {
+    }
+
+    @Test
+    void getDevices() {
+    }
+
+    @Test
+    void start() {
+    }
+
+    @Test
+    void start1() {
+    }
+}
